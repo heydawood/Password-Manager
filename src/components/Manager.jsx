fcc462dded6006ed6cccd006dd31a1e42407d5d5
Dawood Faisal <dawood_faisal@yahoo.com>
2024-05-06 13:21:46 +0500

    commit 5 updated ui, adding savePassword function

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import { useRef, useState, useEffect } from "react"
 
 const Manager = () => {
+    const ref = useRef()
+    const [form, setform] = useState({ site: "", username: "", password: "" })
+    const [passwordArray, setPasswordArray] = useState([])
+
+    useEffect(() => {
+        let passwords = localStorage.getItem("passwords")
+        if (passwords) {
+            setPasswordArray(JSON.parse(passwords))
+        }
+    }, [])
+
+
+    const savePassword = () => {
+        setPasswordArray([...passwordArray, form])
+        localStorage.setItem("password", JSON.stringify([...passwordArray, form]))
+        console.log([...passwordArray, form])
+    }
+
+    const handleChange = (e) => {
+        setform({ ...form, [e.target.name]: e.target.value })
+    }
+
+
+    const showPassword = () => {
+        if (ref.current.src.includes("/eyecross.png")) {
+            ref.current.src = "/eye.png"
+        }
+        else {
+            ref.current.src = "/eyecross.png"
+        }
+    }
     return (
         <>
-            <div class="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div>
+            <div className="absolute inset-0 -z-10 h-full w-full bg-green-50 bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div></div>
 
-            <div className= "mycontainer">
+            <div className="mycontainer">
                 <h1 className='text-4xl font-bold text-center'>
                     <span className='text-green-500'>&lt;</span>
                     Pass
@@ -13,24 +45,53 @@ const Manager = () => {
                 <p className='text-green-900 text-lg text-center'>Your own Password Manager</p>
 
                 <div className="flex flex-col p-4 gap-8 text-black items-center">
-                    <input placeholder='Enter Website URL' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="" id="" />
+                    <input value={form.site} onChange={handleChange} placeholder='Enter Website URL' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="site" id="" />
 
-                    <div  className="flex w-full justify-between gap-8">
-                        <input placeholder='Enter Username' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="" id="" />
+                    <div className="flex w-full justify-between gap-8">
+                        <input value={form.username} onChange={handleChange} placeholder='Enter Username' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="username" id="" />
 
-                        <div className="absolute">
-                        <input placeholder='Enter Password' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="" id="" />
+                        <div className="relative">
+                            <input value={form.password} onChange={handleChange} placeholder='Enter Password' className='rounded-full border border-green-500 w-full p-4 py-1' type="text" name="password" id="" />
+
+                            <span className='absolute right-[3px] top-[4px] cursor-pointer' onClick={showPassword}>
+                                <img ref={ref} className='p-1' width={26} src="/eye.png" alt="eye" />
+                            </span>
+
                         </div>
 
                     </div>
-                    
-                    <button className='flex justify-center items-center gap-2 border border-green-900 bg-green-400 hover:bg-green-500 rounded-full px-8 py-2 w-fit'>
-                    <lord-icon
-                        src="https://cdn.lordicon.com/jgnvfzqg.json"
-                        trigger="hover">
-                    </lord-icon>
-                    Add Password</button>
+
+                    <button onClick={savePassword} className='flex justify-center items-center gap-2 border border-green-900 bg-green-400 hover:bg-green-500 rounded-full px-8 py-2 w-fit'>
+                        <lord-icon
+                            src="https://cdn.lordicon.com/jgnvfzqg.json"
+                            trigger="hover">
+                        </lord-icon>
+                        Add Password</button>
                 </div>
+
+                <div className="passwords">
+                    <h2 className='font-bold text-xl py-3'>Your Saved Passwords</h2>
+                    {passwordArray.length === 0 && <div>No passwords to show</div>} 
+                    {passwordArray.length != 0 && <table className='table-auto w-full rounded-md overflow-hidden'>
+                        <thead className='bg-green-600 text-white'>
+                            <tr>
+                                <th className='py-1'>Site</th>
+                                <th className='py-1'>Username</th>
+                                <th className='py-1'>Password</th>
+                            </tr>
+                        </thead>
+                        <tbody className='bg-green-100'>
+                            {passwordArray.map((item, index)=>{
+                                return <tr key={index}>
+                                <td className='py-1 border border-white text-center w-32'><a href={item.site} target='_blank' >{item.site}</a></td>
+                                <td className='py-1 border border-white text-center w-32'>{item.username}</td>
+                                <td className='py-1 border border-white text-center w-32'>{item.password}</td>
+                            </tr>})}
+                        </tbody>
+                    </table>}
+
+                </div>
+
             </div>
         </>
     )
